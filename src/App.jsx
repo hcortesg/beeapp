78e7fd833ebb4817ce5e34fa1e330eec65cccdbe
hcortesg <humberto.cortes@fuac.edu.co>
2022-12-11 18:02:03 -0500

    Leap year

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@
 import NavBar from './components/NavBar'
 import TaskList from './components/TaskList'
 import TaskForm from './components/TaskForm'
+import LeapForm from './components/LeapForm'
 
 function App() {
  
   return (
     <div className='bg-yellow-200 h-screen'>
       <NavBar/>
-      <TaskForm />
-      <TaskList />
+      <LeapForm/>
+      {/* <TaskForm /> */}
+      {/* <TaskList /> */}
     </div>
   )
 }
